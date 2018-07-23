976d6cb2a9291f3b5f186d185079907eed3f8cc4
peter <designs.peter@gmail.com>
2018-07-23 15:52:44 +0800

    travis 3

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID:
       '89365985355-l0er1iu01ur7pa08uu2rvikm44msct0p.apps.googleusercontent.com',
     googleClientSecret: 'PuTsXBL8dPorBtuDDhDphKDb',
-    mongoURI: 'mongodb:127.0.0.1:27017/blog_ci',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     cookieKey: '123123123',
     redisUrl: 'redis://127.0.0.1:6379'
   };
